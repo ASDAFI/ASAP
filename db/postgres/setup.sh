575fe84f4221ece3c65d87998c23afd2a2c6d0ad
Ali <ali@carriot.ir>
2022-04-08 00:25:09 +0430

    postgres

---
+++ db/postgres/setup.sh
@@ -0,0 +1,2 @@
+make backup-cronjob
+make up
---
+++ db/postgres/setup_cronjob.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir cronjobs
+
+content="
+#!/bin/bash
+docker exec -t postgres pg_dump -h 127.0.0.1 -p 5432 -U asap -w postgres -F p > $(pwd)/backup/postgres.sql
+docker exec -t postgres pg_dump -h 127.0.0.1 -p 5432 -U asap -w asap -F p > $(pwd)/backup/asap.sql
+(date +%s) > $(pwd)/backup/last_recovery.timestamp
+date > $(pwd)/backup/last_recovery.date
+"
+
+
+echo "$content" > backup.sh
+#crontab -l > mycron
+echo "0 */2 * * * bash $(pwd)/backup.sh" > cronjobs/backup.cron
+crontab cronjobs/backup.cron
+
